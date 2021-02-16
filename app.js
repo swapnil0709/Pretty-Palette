843ec7d9e9764e9a85f66d7dd6a702475c958559
swapnil0709 <swapnilsinha20@gmail.com>
2021-02-16 23:49:20 +0530

    Add div color update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ class PrettyPalette {
   constructor() {
     this.color = document.querySelectorAll(".color");
     this.generateBtn = document.querySelector(".generate");
+    this.sliders = document.querySelectorAll("input[type='range']");
   }
 
   generateColors() {
@@ -13,33 +14,87 @@ class PrettyPalette {
     }
     return hash;
     */
-
     return chroma.random(); //using chroma library
   }
   applyColors() {
     this.color.forEach((div, index) => {
       const hexText = div.children[0];
-      const randomColor = colorPalette.generateColors();
+      const randomColor = this.generateColors();
 
       hexText.innerText = randomColor;
       div.style.background = randomColor;
 
       //contrast check
-      colorPalette.checkTextContrast(randomColor, hexText);
+      this.checkTextContrast(randomColor, hexText);
+
+      //   Initialize slider color
+      const color = chroma(randomColor);
+      const sliders = div.querySelectorAll(".sliders input");
+      const [hue, brightness, saturation] = sliders; //destructuring
+
+      this.colorizeSliders(color, hue, brightness, saturation);
     });
   }
   generateButton() {
     this.generateBtn.addEventListener("click", () => {
-      colorPalette.applyColors();
+      this.applyColors();
     });
   }
   checkTextContrast(color, text) {
     const luminance = chroma(color).luminance();
-    console.log(luminance);
+
     text.style.color = luminance > 0.5 ? "black" : "white";
+  }
+  colorizeSliders(color, hue, brightness, saturation) {
+    //   Scale Saturation
+
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+    // Brightness
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+    // Update Input colors
+    saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+      0
+    )},${scaleSat(1)})`;
+
+    brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+      0.5
+    )},${scaleBright(1)})`;
+
+    hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+  }
+
+  hslControls(e) {
+    const index =
+      e.target.getAttribute("data-hue") ||
+      e.target.getAttribute("data-bright") ||
+      e.target.getAttribute("data-sat");
+
+    //Getting each value in a single color div
+    let slider = e.target.parentElement.querySelectorAll("input[type='range']");
+
+    const [hue, brightness, saturation] = slider; //Extracting individual values
+    const bgColor = this.color[index].querySelector("h2").innerText;
+
+    let color = chroma(bgColor)
+      .set("hsl.s", saturation.value)
+      .set("hsl.l", brightness.value)
+      .set("hsl.h", hue.value);
+
+    this.color[index].style.backgroundColor = color;
   }
 }
 
 const colorPalette = new PrettyPalette();
 colorPalette.applyColors();
 colorPalette.generateButton();
+
+colorPalette.sliders.forEach((slider) => {
+  slider.addEventListener("input", (e) => {
+    colorPalette.hslControls(e);
+  });
+});
